5885ec3e1be47e92431d5f56e9e6622ac99f1489
zwan <2zwan@naver.com>
2022-03-28 20:26:18 +0900

    chore: spring init

---
+++ src/main/java/artrun/artrunworker/ArtRunWorkerApplication.java
@@ -0,0 +1,13 @@
+package artrun.artrunworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtRunWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtRunWorkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/artrun/artrunworker/ArtRunWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package artrun.artrunworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtRunWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
